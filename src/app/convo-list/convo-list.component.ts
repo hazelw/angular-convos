42e6f18580ee2fa54eaf5a7e9e29ec072d6a9a87
Hazel Wright <hazel@conversocial.com>
2019-06-10 14:35:22 +0100

    chatz, but angular

---
+++ src/app/convo-list/convo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-convo-list',
+    templateUrl: './convo-list.component.html',
+    styleUrls: ['./convo-list.component.css']
+})
+export class ConvoListComponent {
+    constructor() {}
+}
+
